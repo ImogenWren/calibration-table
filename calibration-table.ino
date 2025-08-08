97ecb2d662224e713d6a23e5d1f75ba4841aefbd
Imogen Wren <imogen.wren91@gmail.com>
2025-08-08 16:07:08 +0100

    finished library ready for use

--- calibration-table.ino
+++ calibration-table.ino
@@ -1,23 +1,98 @@
+/* Calibration-Table
+
+    Example of how to use the calibration.h calibration table library
+
+    
+*/
+
+/*
+Sketch uses 4540 bytes (14%) of program storage space. Maximum is 30720 bytes.
+Global variables use 319 bytes (15%) of dynamic memory, leaving 1729 bytes for local variables. Maximum is 2048 bytes.
+
+Compare this to a cut down & modified version of sensorObject that uses STD::<map> variables, doesnt work as well and uses
+Sketch uses 8518 bytes (27%) of program storage space. Maximum is 30720 bytes.
+Global variables use 516 bytes (25%) of dynamic memory, leaving 1532 bytes for local variables. Maximum is 2048 bytes.
+WAY MORE MEMORY!
+
+I think this libary is winner winner chicken dinner
+*/
+
 
 #include "calibration.h"
+
+// Create Calibration object
+calibration cal;
+
+
+// This calibration table should be filled in with the voltage offset measured at the sensing pin of the microcontroller.
+// This list is arbritary length, limited only by microcontroller memory.
+// To avoid dynamic memory allocation, array size is defined in this header, but can be expanded to allow for longer arrays,
+// Or limit removed entirely to allow dynamic memory allocation> -> maybe at your own risk but you still need to pass the array size
+
+// Create an array of key value pairs to fill with calibration data
+// Enter the key, the raw value measured & calculated for V_at_pin, then enter the offset from true value measured by external measurement device
+// NOTE: this example is not accurate, but is deliberatly all over the place to show correct behaviour of interpolation and further extrapolation
+
+calibration::cal_data cal_map[MAX_ENTRIES] = {
+  { 0.00, 0.00 },
+  { 0.50, -0.022 },
+  { 1.00, -0.035 },
+  { 1.50, -2.063 },
+  { 2.00, -1.100 },
+  { 2.50, -4.124 },
+  { 3.00, -3.143 },
+  { 3.50, -1.180 },
+  { 4.00, -2.213 },
+  { 4.50, -3.250 },
+  { 5.00, -3.270 }
+};
+
+
+/*  Can be used to test sorting function
+calibration::cal_data cal_map[MAX_ENTRIES] = {
+  { 0, 0.00 },
+  { 1, 1 },
+  { 3, 3 },
+  { -2, 2 },
+  { 4, 4 },
+  { -5, 5 },
+  { 7, 7 },
+  { 8, -8 },
+  { 6, 6 },
+  { 9, 9 },
+  { 10, 10 }
+};
+*/
+
 
 
 void setup() {
   Serial.begin(115200);
   Serial.println("input, output");
-  //print_cal_table(cal_map);
+  cal.print_cal_table(cal_map, MAX_ENTRIES);
 
-  float input = -5.0;
+  cal.sort_cal_table(&cal_map[0]);  // cal_map must be passed by pointer to the first element
 
+  cal.print_cal_table(cal_map, MAX_ENTRIES);
 
-  for (int i = 0; i < 150; i++) {
-    float output = do_table_calibration(input, cal_map);
+  float input = -1.0;
+
+  uint32_t startTime = millis();
+
+  while (true) {
+    ;
+  }
+
+  for (int i = 0; i < 500; i++) {
+    float output = cal.do_table_calibration(input, cal_map);
     Serial.print(input);
     Serial.print(", ");
     Serial.println(output);
-    input += 0.1;
-    delay(1);
+    input += 0.03;
   }
+  uint32_t finishTime = millis();
+  Serial.print("time-taken: ");
+  Serial.println(finishTime - startTime);
 }
 
 void loop() {
--- calibration.h
+++ calibration.h
@@ -1,9 +1,21 @@
+/*  Calibration.h
 
+      Multi point generic calibration using key->value pair offset tables
+
+
+
+*/
 
 #pragma once
 
 #ifndef calibration_h
 #define calibration_h
+
+#if (ARDUINO >= 100)
+#include <Arduino.h>
+#else
+#include <wProgram.h>
+#endif
 
 #include <debugging.h>
 
@@ -12,218 +24,88 @@
 // This calibration table should be filled in with the voltage offset measured at the sensing pin of the microcontroller.
 // This list is arbritary length, limited only by microcontroller memory.
 // To avoid dynamic memory allocation, array size is defined in this header, but can be expanded to allow for longer arrays,
-// Or limit removed entirely to allow dynamic memory allocation
+// Or limit removed entirely to allow dynamic memory allocation> -> maybe at your own risk but you still need to pass the array size
+
+/*
+calibration::cal_data cal_map[MAX_ENTRIES] = {
+  { 0.00, 0.00 },
+  { 0.50, -0.022 },
+  { 1.00, -0.035 },
+  { 1.50, -2.063 },
+  { 2.00, -1.100 },
+  { 2.50, -4.124 },
+  { 3.00, -3.143 },
+  { 3.50, -1.180 },
+  { 4.00, -2.213 },
+  { 4.50, -3.250 },
+  { 5.00, -3.270 }
+};
+
+*/
 
 
 // Previous implmentation was done with std::map, howver as this is not available across all arduino implementions, this version will attempt to do the same with simple arrays
 // I think the best option is to use an 1-dimensional array of 2 dimensional arrays or data structures.
 
-#define MAX_ENTRIES 11
-
-// Create a structure to hold key-value pairs
-struct cal_data {
-  float key;
-  float value;
-};
-
-// Create an array of key value pairs to fill with calibration data
-
-// Enter the key, the raw value measured & calculated for V_at_pin, then enter the offset from true value measured by external measurement device
-static cal_data cal_map[MAX_ENTRIES] = {
-  { 0.00, 0.00 },
-  { 0.50, -0.022 },
-  { 1.00, -0.035 },
-  { 1.50, -0.063 },
-  { 2.00, -0.100 },
-  { 2.50, -0.124 },
-  { 3.00, -0.143 },
-  { 3.50, -0.180 },
-  { 4.00, -0.213 },
-  { 4.50, -0.250 },
-  { 5.00, -0.270 }
-};
+#define MAX_ENTRIES 11  // Max entries in calibration table (limited only by memory)
+#define EPSILON 0.1     // Define the tolerance of float comparasons for "exact match"
 
 
-static void print_cal_table(const cal_data *_cal_map, int16_t size = MAX_ENTRIES) {
-  for (int i = 0; i < size; i++) {
-    Serial.print("key: ");
-    Serial.print(_cal_map[i].key);
-    Serial.print(" val: ");
-    Serial.println(_cal_map[i].value);
-  }
-}
-
-/*
- Float comparisons can be imprecise, so consider tolerances (e.g., fabsf(a - b) < EPSILON) if needed.
- - This returns the absolute value of the difference (i,e, always positive) to allow comparason to a tolerance figure EPSILON
- (fabsf(cal_map[i].key - inputVal) < EPSILON )
+/*  Return Value option
+_Should the do_table_calibration function return the corrected input value, or the interpolated value directly from the table, each mode has
+value for different use cases_
 */
 
-// Define the tolerance of float comparasons
-#define EPSILON 0.1
+#define RETURN_CORRECTED_INPUT 0
+#define RETURN_TABLE_VALUE 1
 
+#define RETURN_VALUE_OPTION RETURN_CORRECTED_INPUT
+
+
+/*
+The last problem to solve: how to deal with out of bounds inputs
+
+- Default behaviour is to extrapolate out using the first two datapoints and the last two datapoints
+- Alternative option is to return error code with UNDERSCALE/OVERSCALE  -> program already contains this if errors are enabled
+- Or just use the first/final offset value in the table
+
+- Honestly this is going to add so much code and I want to keep this as light as possible so default behaviour is the only option. Sorry
+
+*/
+
+
+
+class calibration {
+
+public:
+  // Constructor
+  calibration();
+
+  struct cal_data {  // Structure to hold key-value pairs
+    float key;
+    float value;
+  };
+
+
+
+
+
+  void print_cal_table(const cal_data *_cal_map, int16_t size = MAX_ENTRIES);
+
+  void sort_cal_table(cal_data *_cal_map);
+
+  float do_table_calibration(float inputVal, const cal_data *_cal_map);  // table must be sorted!
+
+private:
+
+  uint8_t returnValOption = RETURN_VALUE_OPTION;
+
+
+// bitmasks used internally for tracking state
 #define LOWER_KEY_ENTERED 0b00000001
 #define UPPER_KEY_ENTERED 0b00000010
 #define BOTH_KEYS_ENTERED 0b00000011
-
-static float do_table_calibration(float inputVal, const cal_data *_cal_map) {
-#if DEBUG_LEVEL >= INFO
-  Serial.print("inputVal: ");
-  Serial.println(inputVal, 3);
-#endif
-  float lowerKey;
-  float upperKey;
-  float lowerVal = 0;
-  float upperVal = 0;
-  float lastLowerKey;
-  float lastUpperKey;
-  float lastLowerVal = 0;
-  float lastUpperVal = 0;
-  uint8_t matchState = 0b00000000;  // Used to track what has been matched,
-                                    // can use bitmasks to set and check individual bits,
-                                    //00 = no match, starting sort  01 = lower value found, 10 = upper value found 11 = both values found 100 = exact match (concept, actual implemention might differ)
-                                    //1000 = sort started, -> its important to differentiate because the floats are undefined at the start so useless to compare against themselves
-                                    // This method tracks 4 independent bool states (4 bytes) with 1 8bit variable (1 byte)
-
-  // Search Algorithm
-  for (int i = 0; i < MAX_ENTRIES; ++i) {  //try to find "exact" match - doing some float magic
-    if (fabsf(_cal_map[i].key - inputVal) < EPSILON) {
-      float returnVal = inputVal + _cal_map[i].value;
-#if DEBUG_LEVEL >= INFO
-      Serial.print("key-found: ");
-      Serial.print(_cal_map[i].key, 3);
-      Serial.print(" value: ");
-      Serial.print(_cal_map[i].value, 3);
-      Serial.print(" return-val: ");
-      Serial.println(returnVal, 3);
-#endif
-      return returnVal;
-    } else {                                                        // "exact match" was not found so finding upper and lower values to interpolate
-      if ((matchState & LOWER_KEY_ENTERED) != LOWER_KEY_ENTERED) {  //if any lower key has not been entered
-        if (_cal_map[i].key < inputVal) {                           // get the first key that is lower than input value
-          lowerKey = _cal_map[i].key;
-          lowerVal = _cal_map[i].value;
-          lastLowerKey = lowerKey;
-          lastLowerVal = lowerVal;
-
-#if DEBUG_LEVEL >= INFO
-          Serial.print("initial low key found: ");
-          Serial.print(lowerKey, 3);
-          Serial.print(" lowerVal: ");
-          Serial.println(lowerVal, 3);
-#endif
-          matchState |= LOWER_KEY_ENTERED;  // flag that some data has been captured (though might not be correct yet!)
-        }
-      } else {  // lower key has been entered, so now need to only update if it is HIGHER than existing key, but less than inputVal
-        if (_cal_map[i].key > lowerKey && _cal_map[i].key < inputVal) {
-          lastLowerKey = lowerKey;
-          lowerKey = _cal_map[i].key;
-          lastLowerVal = lowerVal;  // used later if input is offscale
-          lowerVal = _cal_map[i].value;
-#if DEBUG_LEVEL >= INFO
-          Serial.print("low key found: ");
-          Serial.print(lowerKey, 3);
-          Serial.print(" lowerVal: ");
-          Serial.println(lowerVal, 3);
-#endif
-        }
-      }
-      if ((matchState & UPPER_KEY_ENTERED) != UPPER_KEY_ENTERED) {  //if any upper key has not been entered
-        if (_cal_map[i].key > inputVal) {
-          upperKey = _cal_map[i].key;  // get the first key that is higher than input value
-          upperVal = _cal_map[i].value;
-          lastUpperKey = upperKey;
-          lastUpperVal = upperVal;
-#if DEBUG_LEVEL >= INFO
-          Serial.print("initial upper key found: ");
-          Serial.print(upperKey, 3);
-          Serial.print(" upperVal: ");
-          Serial.println(upperVal, 3);
-#endif
-          matchState |= UPPER_KEY_ENTERED;
-        }
-      } else {  // some upper key has been found, so only update if new key is LESS than existing upper key, but higher than inputVal
-        if (_cal_map[i].key < upperKey && _cal_map[i].key > inputVal) {
-          lastUpperKey = upperKey;
-          upperKey = _cal_map[i].key;
-          lastUpperVal = upperVal;
-          upperVal = _cal_map[i].value;
-#if DEBUG_LEVEL >= INFO
-          Serial.print("upper key found: ");
-          Serial.print(upperKey, 3);
-          Serial.print(" upperVal: ");
-          Serial.println(upperVal, 3);
-#endif
-        }
-      }
-    }
-  }  // end of for loop, if function has not returned already, then we must now have an upper and lower key and upper and lower value to do some interpolation with, unless floats are still empty
-     // first check flags again, as an inputVal that is out of bounds of the list should not have been picked up
-  if ((matchState & BOTH_KEYS_ENTERED) != BOTH_KEYS_ENTERED) {
-#if DEBUG_LEVEL >= WARNING
-    Serial.println("inputVal out of range of cal table");
-    Serial.print("matchState: ");
-    Serial.println(matchState, BIN);
-#endif
-    if ((matchState & LOWER_KEY_ENTERED) == LOWER_KEY_ENTERED) {  // Only lower value has been found, therefore inputVal was OVERSCALE, interpolate using highest two values to estimate slope (ALMOST WORKS, need to get the difference)
-#if DEBUG_LEVEL >= WARNING
-      Serial.println("Input OVERSCALE, interpolating using uppermost values to estimate slope");  // and use that to estimate a new high point, the current high point becomes the low point last low point becomes low point
-      Serial.print("lowerKey: ");
-      Serial.print(lowerKey);
-      Serial.print(" lastLowerKey: ");
-      Serial.print(lastLowerKey);
-      Serial.print("lowerVal: ");
-      Serial.print(lowerVal);
-      Serial.print(" lastLowerVal: ");
-      Serial.println(lastLowerVal);
-#endif
-      upperKey = lowerKey;
-      lowerKey = lastLowerKey;
-      upperVal = lowerVal;
-      lowerVal = lastLowerVal;
-      matchState |= UPPER_KEY_ENTERED;
-    } else if ((matchState & UPPER_KEY_ENTERED) == UPPER_KEY_ENTERED) {  // only upper key was found, therefore inputVal was UNDERSCALE, interpolate using lowest two values to estimate slope // THIS ASSUMES SORTED LIST!
-#if DEBUG_LEVEL >= WARNING
-      Serial.println("Input UNDERSCALE, interpolating using lowermost values to estimate slope");
-#endif
-      upperKey = _cal_map[1].key;
-      lowerKey = _cal_map[0].key;
-      upperVal = _cal_map[1].value;
-      lowerVal = _cal_map[0].value;
-      matchState |= LOWER_KEY_ENTERED;
-    } else {
-#if DEBUG_LEVEL >= ERROR
-      Serial.print("No keys were matched, unknown error, this shouldnt even be possible");
-#endif
-    }
-  }
-  // this IF has been changed from an else, now checks again if both keys have been found as the section above to estimate the slope may (should) have found them and set the flags
-  if ((matchState & BOTH_KEYS_ENTERED) == BOTH_KEYS_ENTERED) {
-    float returnVal = lowerVal + (upperVal - lowerVal) * float(inputVal - lowerKey) / fabsf(upperKey - lowerKey);
-#if DEBUG_LEVEL >= INFO
-    Serial.println("Both Keys found, Interpolating...");
-    Serial.print("interpolated offset calculated: ");
-    Serial.println(returnVal, 3);
-#endif
-    returnVal += inputVal;
-#if DEBUG_LEVEL >= INFO
-    Serial.print("corrected inputVal: ");
-    Serial.println(returnVal, 3);
-#endif
-    return returnVal;
-  }
-#if DEBUG_LEVEL >= ERROR
-  Serial.println("Something Went Wrong, returning inputVal");
-#endif
-  return inputVal;  // not found, just return input value
-}
-
-
-
-
-
-
-
+};
 
 
 
